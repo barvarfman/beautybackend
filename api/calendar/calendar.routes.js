ebb861896ceac90e249f39d06865c68b87dda72e
AyalMagid <ayal.magid@gmail.com>
2020-09-10 16:58:58 +0300

    fff

--- api/calendar/calendar.routes.js
+++ api/calendar/calendar.routes.js
@@ -1,14 +1,15 @@
 const express = require('express')
 // const {requireAuth, requireAdmin} = require('../../middlewares/requireAuth.middleware')
-const {getCalendar, deleteCalendar, updateCalendar, addCalendar} = require('./calendar.controller')
+// const {getCalendar, deleteCalendar, updateCalendar, addCalendar} = require('./calendar.controller')
+const {getCalendar} = require('./calendar.controller')
 const router = express.Router()
 
 // middleware that is specific to this router
 // router.use(requireAuth)
 
 router.get('/', getCalendar)
-router.put('/:id',updateCalendar)
-router.delete('/:id', deleteCalendar)
-router.post("/", addCalendar);
+// router.put('/:id',updateCalendar)
+// router.delete('/:id', deleteCalendar)
+// router.post("/", addCalendar);
 
 module.exports = router
