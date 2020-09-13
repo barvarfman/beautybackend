0282cbc44fd97e66fdea4780c595abca3e05da25
AyalMagid <ayal.magid@gmail.com>
2020-09-13 18:50:07 +0300

    ds

--- api/calendar/calendar.service.js
+++ api/calendar/calendar.service.js
@@ -11,9 +11,8 @@ module.exports = {
 async function getCalendar() {
     const calendarId = '413361439'
     try {
-        const calendar = await axios.get(`https://api.kloudless.com/v1/accounts/${calendarId}/cal/calendars`) 
-        console.log(calendar)
-        return calendar 
+        const calendar = await axios.get(`https://api.kloudless.com/v1/accounts/${calendarId}/cal/calendars`)
+        return calendar.objects[0]
     } catch (err) {
             console.log(`ERROR: cannot find calendar`)
         throw err;
