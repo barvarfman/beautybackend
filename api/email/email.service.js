2a6541f263ce09e9b19da121152be65b0014e374
bar varfman <barrestyle@gmail.com>
2020-09-26 15:52:38 +0300

    r

--- api/email/email.service.js
+++ api/email/email.service.js
@@ -1,14 +1,36 @@
-const mailService = require('../../services/SendMail')
+require('dotenv').config();
+
 module.exports = {
     postEmail
 }
+var emailSubject='עדכון תור נייל סלון'
 
-async function postEmail(email) {
-  
-    try {
-      await mailService.SendMailP(email);
-    } catch (err) {
-        console.log('ERROR: cannot find treatments')
-        throw err;
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.OWNER_EMAIL,
+        pass: process.env.PASSWORD
     }
+});
+
+var mailOptions = {
+    from: process.env.OWNER_EMAIL,
+    to: '',
+    subject: emailSubject,
+    text: ''
+};
+
+function postEmail(emailObj) {
+    if (!emailObj.email) mailOptions.to = [process.env.OWNER_EMAIL]
+    else mailOptions.to = [emailObj.email,process.env.OWNER_EMAIL]
+    mailOptions.text = emailObj.bodyText
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
 }
